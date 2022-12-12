92d2b6c260d2786651ec8c8e3a32b8f004772de0
rustehnica <rustehnica@yandex.ru>
2022-12-12 17:03:51 +0700

    footer

--- local/templates/2/components/bitrix/menu_old/template1/template.php
+++ local/templates/2/components/bitrix/menu_old/template1/template.php
@@ -1,37 +1,41 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $this->setFrameMode(true);
 ?>
 
-<?if (!empty($arResult)):?>
+<? if (!empty($arResult)): ?>
 
-<!--Начало главного меню--><ul>
-<? $flag = 0;?>
-<?foreach($arResult as &$menu_item):?>
+    <!--Начало главного меню-->
+    <ul>
+    <? $flag = 0; ?>
+    <? foreach ($arResult as &$menu_item): ?>
 
-	<?if($menu_item['DEPTH_LEVEL'] == 1 && $flag == 1):?>
+        <? if ($menu_item['DEPTH_LEVEL'] == 1 && $flag == 1): ?>
 
-		</ul></li>
-		<?$flag=0;?>
-	<?endif;?>
-		
-	<?if($menu_item['IS_PARENT'] == 1):?>
-		
-		<li class="dropdown" <?if($menu_item['SELECTED'] == 1): echo 'class="active"'; endif;?>>
-			<a data-toggle="dropdown" class="dropdown-toggle-js" href="<?=$menu_item['LINK']?>"><?=$menu_item['TEXT']?></a>
-		<ul class="dropdown-menu">
-		<?$flag = 1;?>
-	
-	<?elseif($menu_item['IS_PARENT'] != 1 && $menu_item['DEPTH_LEVEL'] > 1):?>
-	
-		<li><a href="<?=$menu_item['LINK']?>"><?=$menu_item['TEXT']?></a></li>
-	
-	<?elseif($menu_item['IS_PARENT'] != 1 && $menu_item['DEPTH_LEVEL'] == 1):?>
+            </ul></li>
+            <? $flag = 0; ?>
+        <? endif; ?>
 
-		<li <?if($menu_item['SELECTED'] == 1): echo 'class="active"'; endif;?>><a href="<?=$menu_item['LINK']?>"><?=$menu_item['TEXT']?></a></li>
-	
-	<?endif;?>
-	
-<?endforeach;unset($menu_item);?>
-</ul><?unset($flag);?><!--Конец главного меню-->
+        <? if ($menu_item['IS_PARENT'] == 1): ?>
 
-<?endif;?>
+            <li class="dropdown" <? if ($menu_item['SELECTED'] == 1): echo 'class="active"'; endif; ?>>
+            <a data-toggle="dropdown" class="dropdown-toggle-js"
+               href="<?= $menu_item['LINK'] ?>"><?= $menu_item['TEXT'] ?></a>
+            <ul class="dropdown-menu">
+            <? $flag = 1; ?>
+
+        <? elseif ($menu_item['IS_PARENT'] != 1 && $menu_item['DEPTH_LEVEL'] > 1): ?>
+
+            <li><a href="<?= $menu_item['LINK'] ?>"><?= $menu_item['TEXT'] ?></a></li>
+
+        <? elseif ($menu_item['IS_PARENT'] != 1 && $menu_item['DEPTH_LEVEL'] == 1): ?>
+
+            <li <? if ($menu_item['SELECTED'] == 1): echo 'class="active"'; endif; ?>><a
+                        href="<?= $menu_item['LINK'] ?>"><?= $menu_item['TEXT'] ?></a></li>
+
+        <? endif; ?>
+
+    <? endforeach;
+    unset($menu_item); ?>
+    </ul><? unset($flag); ?><!--Конец главного меню-->
+
+<? endif; ?>
--- local/templates/2/footer.php
+++ local/templates/2/footer.php
@@ -1,125 +1,258 @@
-		</article>
-	</div>
+</article>
+</div>
 </section>
 </div>
+
 <!--noindex-->
-<div style="border:3px solid #eee;padding-top:1rem;">
-<section class="container footer">
+<footer class="container footer" itemscope itemtype="http://schema.org/WPFooter">
 
-	<footer itemscope itemtype="http://schema.org/WPFooter">
-		
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-6"><a href="/"><img src="/bitrix/templates/2/img/logo.png" alt="Завод трансформаторного оборудования &quot;Арктика&quot;"></a></div>
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-6">
-			Вы принимаете условия <a href="/politika-konfidentsialnosti/">политики конфиденциальности</a> и пользовательского соглашения каждый раз, когда оставляете свои данные в любой форме обратной связи на сайте tszi.ru. <a href="/mcatalog/">Метки</a></div></div>
-	
+    <div class="row mt-2">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <a href="/">
+                <img src="/bitrix/templates/2/img/logo.png" alt="Завод трансформаторного оборудования &quot;Арктика&quot;">
+            </a>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fb fs-18">г. Барнаул, проезд Южный, 31Г, корпус 1</div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-1 fb fs-18">8 800 301 79 59 (отдел продаж)<br>transformator1@ruteh.ru
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-1 fb fs-18">8 800 301 79 59 (производство)<br>transformator1@ruteh.ru
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 hidden-xs">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fs-25"><a class="fc-black" href="/about/">Компания</a></div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-1 fs-25"><a class="fc-black" href="/catalog/uslugi/">Услуги</a></div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-1 fs-25"><a class="fc-black" href="/tendery/">Тендеры</a></div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 hidden-xs">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fs-25"><a class="fc-black" href="/catalog/">Каталог</a></div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-1 fs-18">
+                    <a class="fc-gray" href="/about/o-zavode/">О заводе</a><br>
+                    <a class="fc-gray" href="/about/tekhnologii/">Технологии</a><br>
+                    <a class="fc-gray" href="/about/premushchestva-sotrudnichestva/">Преимущества</a><br>
+                    <a class="fc-gray" href="/reviews/">Отзывы</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 hidden-xs">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fs-25">Информация</div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-1 fs-18"><a class="fc-gray" href="/dostavka/">Условия доставки</a><br><a class="fc-gray" href="/oplata/">Условия оплаты</a></div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 hidden-xs">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fs-25">Помощь</div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-1 fs-18"><a class="fc-gray" href="/faq/">Вопрос-Ответ</a><br><a class="fc-gray" href="/poddelka/">Подделки</a></div>
+            </div>
+        </div>
+    </div>
 
-	</footer>
-	
-</section>
-</div>
+    <div class="row mt-3 hidden-xs">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-12">
+            Вы принимаете условия <a href="/politika-konfidentsialnosti/">политики конфиденциальности</a> и
+            пользовательского соглашения каждый раз, когда оставляете свои данные в любой форме обратной связи на
+            сайте tszi.ru. <a href="/mcatalog/">Метки</a>
+        </div>
+    </div>
 
-
-
+</footer>
 
 <div class="menuright hidden-xs">
 
-	<?$APPLICATION->IncludeComponent(
-		"bitrix:menu",
-		"menuright",
-		Array(
-			"ROOT_MENU_TYPE" => "right",
-			"MENU_CACHE_TYPE" => "A",
-			"MENU_CACHE_TIME" => "3600",
-			"MENU_CACHE_USE_GROUPS" => "N",
-			"MENU_CACHE_GET_VARS" => array(""),
-			"MAX_LEVEL" => "2",
-			"CHILD_MENU_TYPE" => "left_child",
-			"USE_EXT" => "Y",
-			"DELAY" => "N",
-			"ALLOW_MULTI_SELECT" => "N",
-			"MENU_THEME" => "site"
-		)
-	);?>
-	
+    <? $APPLICATION->IncludeComponent(
+        "bitrix:menu",
+        "menuright",
+        array(
+            "ROOT_MENU_TYPE" => "right",
+            "MENU_CACHE_TYPE" => "A",
+            "MENU_CACHE_TIME" => "3600",
+            "MENU_CACHE_USE_GROUPS" => "N",
+            "MENU_CACHE_GET_VARS" => array(""),
+            "MAX_LEVEL" => "2",
+            "CHILD_MENU_TYPE" => "left_child",
+            "USE_EXT" => "Y",
+            "DELAY" => "N",
+            "ALLOW_MULTI_SELECT" => "N",
+            "MENU_THEME" => "site"
+        )
+    ); ?>
+
 </div>
 
 
 <!--/noindex-->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
-	<!--??????????? js-->	
-	<div style="display: none;max-width:600px;" id="trueModal">
-		<?$APPLICATION->IncludeComponent(
-			"bitrix:main.feedback",
-			"main_feedback",
-			Array(
-		"AJAX_MODE" => "Y",
-				"AJAX_OPTION_SHADOW" => "N",
-				"AJAX_OPTION_JUMP" => "N",
-				"AJAX_OPTION_STYLE" => "Y",
-				"AJAX_OPTION_HISTORY" => "N",
-				"EMAIL_TO" => "transformator1@ruteh.ru",
-				"EVENT_MESSAGE_ID" => array("5"),
-				"OK_TEXT" => "Спасибо, ваше сообщение принято.",
-				"REQUIRED_FIELDS" => array("NAME","EMAIL"),
-				"USE_CAPTCHA" => "Y"
-			)
-		);?>
-	</div>
-<script>(function(t, p) {window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})('Widget', {id: '5e57917d8f09ba00440aef78', position: 'left', delay: 10, autoOpen: 10, disableOnMobile: true})</script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css"/>
+<!--??????????? js-->
+<div style="display: none;max-width:600px;" id="trueModal">
+    <? $APPLICATION->IncludeComponent(
+        "bitrix:main.feedback",
+        "main_feedback",
+        array(
+            "AJAX_MODE" => "Y",
+            "AJAX_OPTION_SHADOW" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "AJAX_OPTION_HISTORY" => "N",
+            "EMAIL_TO" => "transformator1@ruteh.ru",
+            "EVENT_MESSAGE_ID" => array("5"),
+            "OK_TEXT" => "Спасибо, ваше сообщение принято.",
+            "REQUIRED_FIELDS" => array("NAME", "EMAIL"),
+            "USE_CAPTCHA" => "Y"
+        )
+    ); ?>
+</div>
+<script>(function (t, p) {
+        window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function () {
+            Marquiz.add([t, p])
+        })
+    })('Widget', {
+        id: '5e57917d8f09ba00440aef78',
+        position: 'left',
+        delay: 10,
+        autoOpen: 10,
+        disableOnMobile: true
+    })</script>
 <!-- calltouch -->
 <script type="text/javascript">
-(function(w,d,n,c){w.CalltouchDataObject=n;w[n]=function(){w[n]["callbacks"].push(arguments)};if(!w[n]["callbacks"]){w[n]["callbacks"]=[]}w[n]["loaded"]=false;if(typeof c!=="object"){c=[c]}w[n]["counters"]=c;for(var i=0;i<c.length;i+=1){p(c[i])}function p(cId){var a=d.getElementsByTagName("script")[0],s=d.createElement("script"),i=function(){a.parentNode.insertBefore(s,a)};s.type="text/javascript";s.async=true;s.src="https://mod.calltouch.ru/init.js?id="+cId;if(w.opera=="[object Opera]"){d.addEventListener("DOMContentLoaded",i,false)}else{i()}}})(window,document,"ct","44a3twpb");
+    (function (w, d, n, c) {
+        w.CalltouchDataObject = n;
+        w[n] = function () {
+            w[n]["callbacks"].push(arguments)
+        };
+        if (!w[n]["callbacks"]) {
+            w[n]["callbacks"] = []
+        }
+        w[n]["loaded"] = false;
+        if (typeof c !== "object") {
+            c = [c]
+        }
+        w[n]["counters"] = c;
+        for (var i = 0; i < c.length; i += 1) {
+            p(c[i])
+        }
+
+        function p(cId) {
+            var a = d.getElementsByTagName("script")[0], s = d.createElement("script"), i = function () {
+                a.parentNode.insertBefore(s, a)
+            };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mod.calltouch.ru/init.js?id=" + cId;
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", i, false)
+            } else {
+                i()
+            }
+        }
+    })(window, document, "ct", "44a3twpb");
 </script>
 
 <script type="text/javascript">
-var _ctreq_b24 = function(data) {
-    var sid = 44967;
-    var request = window.ActiveXObject?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();
-    var post_data = Object.keys(data).reduce(function(a,k){if(!!data[k]){a.push(k+'='+encodeURIComponent(data[k]));}return a},[]).join('&');
-    var url = 'https://api.calltouch.ru/calls-service/RestAPI/'+sid+'/requests/orders/register/';
-    request.open("POST", url, true); request.setRequestHeader('Content-Type','application/x-www-form-urlencoded'); request.send(post_data);
-};
-window.addEventListener('b24:form:submit', function(e){
-    var form = event.detail.object;
-    if (form.validated){
-        var fio = ''; var phone = ''; var email = ''; var comment = '';
-        form.getFields().forEach(function(el){
-            if(el.name=='LEAD_NAME' || el.name=='CONTACT_NAME'){ fio = el.value(); }
-            if(el.name=='LEAD_PHONE' || el.name=='CONTACT_PHONE'){ phone = el.value(); }
-            if(el.name=='LEAD_EMAIL' || el.name=='CONTACT_EMAIL'){ email = el.value(); }
-            if(el.name=='LEAD_COMMENTS' || el.name=='DEAL_COMMENTS '){ comment = el.value(); }
-        });
-        var sub = 'Заявка с формы Bitrix24 ' + location.hostname;
-        var ct_data = {fio: fio, phoneNumber: phone, email: email, comment: comment, subject: sub, requestUrl: location.href, sessionId: window.call_value};
-        console.log(ct_data);
-        if (!!phone || !!email) _ctreq_b24(ct_data);
-    }
-});
-</script>
-<script>
-window.addEventListener('onBitrixLiveChat', function(event){
-    var widget = event.detail.widget;
-    widget.subscribe({
-        type: BX.LiveChatWidget.SubscriptionType.every, callback: function(ev){
-            if (ev.type == BX.LiveChatWidget.SubscriptionType.userMessage){
-                if (!window.ct_snd_flag && !!window.ct){ ct('goal','b24chat'); window.ct_snd_flag = 1; }
+    var _ctreq_b24 = function (data) {
+        var sid = 44967;
+        var request = window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
+        var post_data = Object.keys(data).reduce(function (a, k) {
+            if (!!data[k]) {
+                a.push(k + '=' + encodeURIComponent(data[k]));
             }
-            if (ev.type == BX.LiveChatWidget.SubscriptionType.userForm){
-                var sid = 44967;
-                var fio = ''; var phone = ''; var email = '';
-                if (!!ev.data && !!ev.data.fields && !!ev.data.fields.name){ fio = ev.data.fields.name; }
-                if (!!ev.data && !!ev.data.fields && !!ev.data.fields.phone){ phone = ev.data.fields.phone; }
-                if (!!ev.data && !!ev.data.fields && !!ev.data.fields.email){ email = ev.data.fields.email; }
-                var ct_data = {}; ct_data.phoneNumber = phone; ct_data.fio = fio; ct_data.email = email; ct_data.requestUrl = location.href; ct_data.sessionId = window.call_value;
-                ct_data.subject = 'Чат Bitrix24 посетитель оставил контакты';
-                var request = window.ActiveXObject?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();
-                var post_data = Object.keys(ct_data).reduce(function(a,k){if(!!ct_data[k]){a.push(k+'='+encodeURIComponent(ct_data[k]));}return a},[]).join('&');
-                var url = 'https://api.calltouch.ru/calls-service/RestAPI/'+sid+'/requests/orders/register/';
-                request.open("POST", url, true); request.setRequestHeader('Content-Type','application/x-www-form-urlencoded'); request.send(post_data);
-            }
+            return a
+        }, []).join('&');
+        var url = 'https://api.calltouch.ru/calls-service/RestAPI/' + sid + '/requests/orders/register/';
+        request.open("POST", url, true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.send(post_data);
+    };
+    window.addEventListener('b24:form:submit', function (e) {
+        var form = event.detail.object;
+        if (form.validated) {
+            var fio = '';
+            var phone = '';
+            var email = '';
+            var comment = '';
+            form.getFields().forEach(function (el) {
+                if (el.name == 'LEAD_NAME' || el.name == 'CONTACT_NAME') {
+                    fio = el.value();
+                }
+                if (el.name == 'LEAD_PHONE' || el.name == 'CONTACT_PHONE') {
+                    phone = el.value();
+                }
+                if (el.name == 'LEAD_EMAIL' || el.name == 'CONTACT_EMAIL') {
+                    email = el.value();
+                }
+                if (el.name == 'LEAD_COMMENTS' || el.name == 'DEAL_COMMENTS ') {
+                    comment = el.value();
+                }
+            });
+            var sub = 'Заявка с формы Bitrix24 ' + location.hostname;
+            var ct_data = {
+                fio: fio,
+                phoneNumber: phone,
+                email: email,
+                comment: comment,
+                subject: sub,
+                requestUrl: location.href,
+                sessionId: window.call_value
+            };
+            console.log(ct_data);
+            if (!!phone || !!email) _ctreq_b24(ct_data);
         }
     });
-});
+</script>
+<script>
+    window.addEventListener('onBitrixLiveChat', function (event) {
+        var widget = event.detail.widget;
+        widget.subscribe({
+            type: BX.LiveChatWidget.SubscriptionType.every, callback: function (ev) {
+                if (ev.type == BX.LiveChatWidget.SubscriptionType.userMessage) {
+                    if (!window.ct_snd_flag && !!window.ct) {
+                        ct('goal', 'b24chat');
+                        window.ct_snd_flag = 1;
+                    }
+                }
+                if (ev.type == BX.LiveChatWidget.SubscriptionType.userForm) {
+                    var sid = 44967;
+                    var fio = '';
+                    var phone = '';
+                    var email = '';
+                    if (!!ev.data && !!ev.data.fields && !!ev.data.fields.name) {
+                        fio = ev.data.fields.name;
+                    }
+                    if (!!ev.data && !!ev.data.fields && !!ev.data.fields.phone) {
+                        phone = ev.data.fields.phone;
+                    }
+                    if (!!ev.data && !!ev.data.fields && !!ev.data.fields.email) {
+                        email = ev.data.fields.email;
+                    }
+                    var ct_data = {};
+                    ct_data.phoneNumber = phone;
+                    ct_data.fio = fio;
+                    ct_data.email = email;
+                    ct_data.requestUrl = location.href;
+                    ct_data.sessionId = window.call_value;
+                    ct_data.subject = 'Чат Bitrix24 посетитель оставил контакты';
+                    var request = window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
+                    var post_data = Object.keys(ct_data).reduce(function (a, k) {
+                        if (!!ct_data[k]) {
+                            a.push(k + '=' + encodeURIComponent(ct_data[k]));
+                        }
+                        return a
+                    }, []).join('&');
+                    var url = 'https://api.calltouch.ru/calls-service/RestAPI/' + sid + '/requests/orders/register/';
+                    request.open("POST", url, true);
+                    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    request.send(post_data);
+                }
+            }
+        });
+    });
 </script>
 <!-- calltouch -->
 </body>
